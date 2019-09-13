c0dc0ef7d61f0f7828edc52de9abd0e3410ff3c1
github username <github email address>
2019-09-13 12:32:14 -0400

    submit round 2 code challenge

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,27 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    if params[:search]
+      @restaurants = Restaurant.where('name LIKE ?', "#{params[:search]}")
+    else
+      @restaurants = Restaurant.all
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to @restaurant
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :search)
   end
 end
